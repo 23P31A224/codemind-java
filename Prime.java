7fc88b596d7d5f1b42980c34b6de6ed824a349be
Likhitha G <147792019+23P31A224@users.noreply.github.com>
2024-08-06 22:28:16 +0530

    import java.util.Scanner; public class prime{     public static void main(String[] args){         int n;         Scanner read=new Scanner(System.in);         n=read.nextInt();         int i,fact=0;         for(i=1;i<=n;i++){             if(n%i==0){                 fact=fact+1;             }         }         if(fact==2){             System.out.println("Prime");         }         else{             System.out.println("Not Prime");         }     } }

---
+++ Prime.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class prime{
+    public static void main(String[] args){
+        int n;
+        Scanner read=new Scanner(System.in);
+        n=read.nextInt();
+        int i,fact=0;
+        for(i=1;i<=n;i++){
+            if(n%i==0){
+                fact=fact+1;
+            }
+        }
+        if(fact==2){
+            System.out.println("Prime");
+        }
+        else{
+            System.out.println("Not Prime");
+        }
+    }
+}
