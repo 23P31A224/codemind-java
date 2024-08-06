90120e7c13fbcb06c074388937e9d2c7b1ffa8ad
Likhitha G <147792019+23P31A224@users.noreply.github.com>
2024-08-06 22:11:00 +0530

    import java.util.Scanner;
    public class sumoffirstNnaaturalnumbers{
        public static void main(String[] args){
            int a;
            Scanner read=new Scanner(System.in);
                a=read.nextInt();
                int i;
                for(i=1;i<=a;i++){
                System.out.printf("%d ",i);
                }
            }
        }

---
+++ 1_to_n.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class sumoffirstNnaaturalnumbers{
+    public static void main(String[] args){
+        int a;
+        Scanner read=new Scanner(System.in);
+            a=read.nextInt();
+            int i;
+            for(i=1;i<=a;i++){
+            System.out.printf("%d ",i);
+            }
+        }
+    }
