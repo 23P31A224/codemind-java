e917e521eacce7fac4231424696e052d37c42778
Likhitha G <147792019+23P31A224@users.noreply.github.com>
2024-08-06 22:08:17 +0530

    import java.util.Scanner;
    public class sumoffirstNnaaturalnumbers{
        public static void main(String[] args){
            int n;
            Scanner read=new Scanner(System.in);
                n=read.nextInt();
                int i;
                for(i=n;i>=1;i--){
                System.out.printf("%d ",i);
                }
            }
        }

---
+++ N_to_1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class sumoffirstNnaaturalnumbers{
+    public static void main(String[] args){
+        int n;
+        Scanner read=new Scanner(System.in);
+            n=read.nextInt();
+            int i;
+            for(i=n;i>=1;i--){
+            System.out.printf("%d ",i);
+            }
+        }
+    }
