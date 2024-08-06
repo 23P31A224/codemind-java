ccf5bcd0c974483f23f21bd02d117090477f695b
Likhitha G <147792019+23P31A224@users.noreply.github.com>
2024-08-06 22:21:56 +0530

    import java.util.Scanner;
    public class sumoffirstNnaaturalnumbers{
        public static void main(String[] args){
            int a;
            Scanner read=new Scanner(System.in);
                a=read.nextInt();
                int i,j;
                for(i=1;i<=a;i++){
                    for(j=1;j<=i;j++){
                System.out.printf("*");
                }
                System.out.printf("
    ");
                }
                
            }
        }

---
+++ printing_pattern_like_a_right-angle_triangle_with_a_little_star.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class sumoffirstNnaaturalnumbers{
+    public static void main(String[] args){
+        int a;
+        Scanner read=new Scanner(System.in);
+            a=read.nextInt();
+            int i,j;
+            for(i=1;i<=a;i++){
+                for(j=1;j<=i;j++){
+            System.out.printf("*");
+            }
+            System.out.printf("
+");
+            }
+            
+        }
+    }
